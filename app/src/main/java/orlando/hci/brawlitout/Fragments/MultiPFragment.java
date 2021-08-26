3f3440b8879f27ef1f53625f3fc4ed1cc18e5909
orlan <orlandoalessio1998@gmail.com>
2021-08-26 19:11:46 +0200

    First Fragment Started Multi-Fragment

--- app/src/main/java/orlando/hci/brawlitout/Fragments/MultiPFragment.java
+++ app/src/main/java/orlando/hci/brawlitout/Fragments/MultiPFragment.java
@@ -1,29 +1,31 @@
 package orlando.hci.brawlitout.Fragments;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import orlando.hci.brawlitout.Adapters.PlayerAdapter;
+import orlando.hci.brawlitout.Utils.Player;
 import orlando.hci.brawlitout.R;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class MultiPFragment extends Fragment {
+
+    private ArrayList<Player> players = new ArrayList<>();
+    private int playercount = 0;
+    private Button ok_btn;
+    private EditText nplayer_edit;
+    private PlayerAdapter platerAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,8 +35,29 @@ public class MultiPFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_multi, container, false);
+        ok_btn = root.findViewById(R.id.ok_btn);
+        nplayer_edit = root.findViewById(R.id.edit_nplayer);
+        this.platerAdapter = new PlayerAdapter(players);
+        RecyclerView recyclerView = root.findViewById(R.id.player_recyclerview);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setAdapter(platerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        ok_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playercount = Integer.parseInt(nplayer_edit.getText().toString());
+                for (int i = 0; i < playercount; i++) {
+                    players.add(new Player(i, "Player " + (i+1)));
+                    Toast.makeText(requireActivity(), "AO", Toast.LENGTH_SHORT).show();
+                    platerAdapter.notifyItemInserted(i);
+                }
+
+            }
+        });
+
 
         return root;
     }
-
 }
--- app/src/main/java/orlando/hci/brawlitout/Fragments/SinglePFragment.java
+++ app/src/main/java/orlando/hci/brawlitout/Fragments/SinglePFragment.java
@@ -1,5 +1,6 @@
 package orlando.hci.brawlitout.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -7,6 +8,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,10 +20,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import orlando.hci.brawlitout.R;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class SinglePFragment extends Fragment {
+
+
+    private long starttime;
+    private long endtime;
+    private double difference;
+    private boolean is_running;
+    private Button ss_btn;
+    private Button r_btn;
+    private TextView result;
 
 
     @Override
@@ -32,6 +46,58 @@ public class SinglePFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_single, container, false);
 
+
+        ss_btn = (Button) root.findViewById(R.id.button_start_pause);
+        r_btn = (Button) root.findViewById(R.id.button_reset);
+        result = (TextView) root.findViewById(R.id.chronometer);
+
+        ss_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (is_running)  // condition on which you check whether it's start or stop
+                {
+                    onClickStop(v);
+                } else {
+                    onClickStart(v);
+                }
+            }
+        });
+
+        r_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onClickReset(v);
+            }
+        });
+
         return root;
+
     }
+
+
+    private void onClickStart(View view) {
+        starttime = System.nanoTime();
+        ss_btn.setText(R.string.stop);
+        is_running = true;
+    }
+
+    private void onClickStop(View view) {
+        endtime = System.nanoTime();
+        ss_btn.setText(R.string.start);
+        is_running = false;
+        difference = (endtime - starttime) / 1e3;
+        updateTime(difference/ 1e3);
+    }
+
+    private void onClickReset(View view) {
+        starttime = 0;
+        endtime = 0;
+        ss_btn.setText(R.string.start);
+        result.setText("");
+    }
+
+    private void updateTime(double updatedTime) {
+        DateFormat format = new SimpleDateFormat("ss.SSS");
+        String displayTime = format.format(updatedTime);
+        result.setText(displayTime);
+    }
+
 }
