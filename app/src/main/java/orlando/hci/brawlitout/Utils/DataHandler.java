b47a2090dc627cf8481344cf14b083f645e5a575
michele17284 <65226360+michele17284@users.noreply.github.com>
2021-08-26 19:16:24 +0200

    add data handler to save the scores

---
+++ app/src/main/java/orlando/hci/brawlitout/Utils/DataHandler.java
@@ -0,0 +1,40 @@
+package com.example.androidaccelerometerexample;
+
+
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Map;
+
+public class DataHandler extends AppCompatActivity {
+
+    public static final String SHARED_PREFERENCES = "sharedPrefs";
+    public void add(String name, Float milliseconds){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat(name,milliseconds);
+        editor.apply();
+        Toast.makeText(this,"Data saved!",Toast.LENGTH_SHORT).show();
+
+    }
+
+    public Map<String, ?> load(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES,MODE_PRIVATE);
+        return sharedPreferences.getAll();
+    }
+
+    public void remove(String key){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove(key);
+    }
+
+    public void clear(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+    }
+
+}
